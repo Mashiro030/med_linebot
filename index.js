b464a475d083d5fd0e26cbd5e9a62c0740971508
邵柏鈞 <46882597+Mashiro030@users.noreply.github.com>
2019-04-28 23:06:18 +0800

    抓取lineapli的timestamp

--- index.js
+++ index.js
@@ -4,71 +4,78 @@ const util = require("util")
 const line = require('@line/bot-sdk');
 const express = require("express");
 const request = require("request");
-const configGet= require('config');
+const configGet = require('config');
 const app = express();
 const mysql = require("mysql");
 const db = mysql.createConnection({
-  host: "medical.cg1fvo9lgals.ap-southeast-1.rds.amazonaws.com",
-  user: "admin",
-  password: "12345678",
-  database: "medical"
+    host: "medical.cg1fvo9lgals.ap-southeast-1.rds.amazonaws.com",
+    user: "admin",
+    password: "12345678",
+    database: "medical"
 });
 db.connect();
 
 const bot = linebot({
-  channelId: '1561441777',
-  channelSecret: configGet.get("CHANNEL_SECRET"),
-  channelAccessToken: configGet.get("CHANNEL_ACCESS_TOKEN")
+    channelId: '1561441777',
+    channelSecret: configGet.get("CHANNEL_SECRET"),
+    channelAccessToken: configGet.get("CHANNEL_ACCESS_TOKEN")
 });
 const linebotParser = bot.parser();
 
-app.post('/webhook', linebotParser,function (req, res) {
+app.post('/webhook', linebotParser, function (req, res) {
     console.log('webhook in')
 });
 
+var time = [];
 
 bot.on('message', function (event) {
-    if(event.message.text=='查詢'){
+    if (event.message.text == '查詢') {
         console.log(event.message.text);
-        event.reply({ type: 'text', text: '請輸入身分證已進行查詢: '});
-        db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
-            // console.log(results);
-            if (results.length == false) {
-                event.reply("查無掛號紀錄，請輸入掛號進行預約，謝謝。");
-            } else {
-                console.log(results[0]);
-                event.reply(JSON.stringify(results[0]));
-            }
-        });
+        event.reply({ type: 'text', text: '請輸入身分證已進行查詢: ' })
+        console.log(time.push(event.timestamp))
+        console.log(time)
+        time = []
+        console.log(time)
     }
-    if(event.message.text=='掛號'){
+    if (event.message.text == '掛號') {
         console.log(event.message.text)
         event.reply({ type: 'text', text: '請問你要掛哪一科?請輸入牙科或胸腔科' });
-        console.log(event)
-    } 
-    if(event.message.text=='牙科'){
-        console.log(event.message.text)
-        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: '})
+        console.log(time.push(event.timestamp))
+        console.log(time)
+        time = []
+        console.log(time)
     }
-    if(event.message.text=='胸腔科'){
+    if (event.message.text == '牙科') {
         console.log(event.message.text)
-        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: '})
+        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: ' })
+        console.log(time.push(event.timestamp))
+        console.log(time)
+        time = []
+        console.log(time)
+    }
+    if (event.message.text == '胸腔科') {
+        console.log(event.message.text)
+        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: ' })
+        console.log(time.push(event.timestamp))
+        console.log(time)
+        time = []
+        console.log(time)
     }
 });
 
 
 
 
-    // db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
-    //     // console.log(results);
-    //     if (results.length == false) {
-    //         console.log('wrong input');
-    //         event.reply("wrong input");
-    //     } else {
-    //         console.log(results[0]);
-    //         event.reply(JSON.stringify(results[0]));
-    //     }
-    // });
+// db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
+//     // console.log(results);
+//     if (results.length == false) {
+//         console.log('wrong input');
+//         event.reply("wrong input");
+//     } else {
+//         console.log(results[0]);
+//         event.reply(JSON.stringify(results[0]));
+//     }
+// });
 
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
