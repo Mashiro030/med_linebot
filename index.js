eeb31618730f213822dc394ead81b413cd10aeb7
邵柏鈞 <123@gmail.com>
2019-04-29 16:58:04 +0800

    完成掛號、查詢的資料庫獨立

--- index.js
+++ index.js
@@ -27,71 +27,57 @@ app.post('/webhook', linebotParser, function (req, res) {
     console.log('webhook in')
 });
 
-var time = [];
-var text = [];
+var appointment = [];
+var search = [];
+
 bot.on('message', function (event) {
+    var userinput = event.message.text;
     if (event.message.text == '查詢') {
+        search.push(event.timestamp)
+        console.log(search.length)
         console.log(event.message.text);
         event.reply({ type: 'text', text: '請輸入身分證已進行查詢: ' })
-        time.push(event.timestamp)
-        console.log(time)
-        time = []
-        console.log(time)
-        // console.log(event)
-    }
-    if (event.message.text == '掛號') {
-        console.log(event.message.text)
-        event.reply({ type: 'text', text: '請問你要掛哪一科?請輸入牙科或胸腔科' });
-        console.log(time.push(event.timestamp))
-        console.log(time)
-        time = []
-        console.log(time)
-    }
-    if (event.message.text == '牙科') {
-        console.log(event.message.text)
-        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: ' })
-    }
-    if (event.message.text == '胸腔科') {
-        console.log(event.message.text)
-        event.reply({ type: 'text', text: '請輸入身分證已進行驗證: ' })
-        console.log(time.push(event.timestamp))
-        console.log(time)
-        time = []
-        console.log(time)
-    }
-});
-
-bot.on('message', function (event){
-    db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
-            text.push(event.message.text)
-            console.log(text)
-            text=[]
-            console.log(text)
+    }else if(userinput.match(/^[A-Z]\d{9}$/)&& search.length>=1){
+        db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
             if (results.length == false) {
-                event.reply({ type: 'text', text: '請輸入身分證已進行操作' });
+                event.reply({ type: 'text', text: '輸入錯誤' });
             }
             else {
                 console.log(results[0])
                 console.log(results[0].Doctor);
-                event.reply([{type: 'text', text: '掛號資訊:'},{ type: 'text', text: '醫生:'+results[0].Doctor},{type: 'text', text: '科別:'+results[0].Subject},{type: 'text', text: '看診時間:'+moment(results[0].App_time).format('YYYY/MM/DD hh:mm')}]);
-                // event.reply({ type: 'text', text: '醫生:'+results[0].Doctor});
+                event.reply({type:''})
+                event.reply([{type: 'text', text: '掛號資訊:'},{ type: 'text', text: '醫生:'+results[0].Doctor},{type: 'text', text: '科別:'+results[0].Subject},{type: 'text', text: '看診時間:'+moment(results[0].App_time).format('YYYY/MM/DD hh:mm')},{type:'text',text:'查詢成功'}]);
             }
         });
-    });
+        search=[]
+        console.log(search.length)
+    }
+    if (userinput == '掛號') {
+        appointment.push(event.timestamp)
+        console.log(appointment.length)
+        console.log(event.message.text)
+        event.reply({ type: 'text', text: '請問你要掛哪一科?請輸入牙科或胸腔科' });
+    }else if(userinput=='牙科'||userinput=='胸腔科'){
+        event.reply({type:'text',text:'請輸入身分證字號、姓名、科別、預約時間以進行掛號:'})
+    }else if(userinput.match(/^[A-Z]\d{9}$/) && appointment.length>=1 ){
+        db.query("SELECT ID,Name,Subject,APP_time FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
+            if (results.length == true) {
+                event.reply({ type: 'text', text: '你有已掛號的紀錄，請輸入查詢以檢視相關資訊' });
+            }
+            else if(results.length == false){
+                event.reply({type:'text',text:'請一次輸入:1.身分證2.姓名3.性別4.科別5.預約時間6.醫生'});
+                let val = {ID:userinput}
+                
+                db.query("INSERT INTO `med_appointment_sub` set=?",)
+                
+            } 
+        });
+        appointment=[]
+        console.log(appointment.length)
+    }
 
+});
 
-
-
-// db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
-//     // console.log(results);
-//     if (results.length == false) {
-//         console.log('wrong input');
-//         event.reply("wrong input");
-//     } else {
-//         console.log(results[0]);
-//         event.reply(JSON.stringify(results[0]));
-//     }
-// });
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
