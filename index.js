aaafd015f7cc5cb889ebc23d0ca722cdecb22607
邵柏鈞 <46882597+Mashiro030@users.noreply.github.com>
2019-05-02 23:19:08 +0800

    加入權證

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ const bot = linebot({
 });
 const linebotParser = bot.parser();
 
+//底下輸入client_secret.json檔案的內容
+var myClientSecret={"installed":{"client_id":"301937700301-moqnki2rg80fgfokld1urbiai12o4o2p.apps.googleusercontent.com","project_id":"sunny-jetty-239408","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"zvVPMg3s_WywXsGLrvVSclrY","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+
+var auth = new googleAuth();
+var oauth2Client = new auth.OAuth2(myClientSecret.installed.client_id,myClientSecret.installed.client_secret, myClientSecret.installed.redirect_uris[0]);
+
+//底下輸入sheetsapi.json檔案的內容
+oauth2Client.credentials ={"access_token":"ya29.Glv9BhApw9q81SgwZlWHEtAANaK94ZDnN3mBCB4Aowz6t5sZBECrlvmvsexRaIyh4v-28YOGj1gmWvzyBuoyC5r43atLcaeHWP8WazAD93ZLEinuN06SPczmsxGa","refresh_token":"1/P-XYiLeyOedrTzD_AKHRVMFX_rV3yERSb4wi4_14-ks","scope":"https://www.googleapis.com/auth/spreadsheets.readonly","token_type":"Bearer","expiry_date":1556813212607}
+
+//試算表的ID，引號不能刪掉
+var mySheetId='1iqZWdfswYPzuAiXt5iren_zWndUqeLk5s3G76lwH-zY';
+
 app.post('/webhook', linebotParser, function (req, res) {
     console.log('webhook in')
 });
