224d6cd1b9cab5f5d52985b0103b80f8d847d132
邵柏鈞 <123@gmail.com>
2019-05-13 19:27:35 +0800

    嘗試把值存起來

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const google = require('googleapis');
 const  googleAuth = require('google-auth-library');
 const db = mysql.createConnection({
     host: "medical.cg1fvo9lgals.ap-southeast-1.rds.amazonaws.com",
-    user: "admin",
-    password: "12345678",
+    user: "administrator",
+    password: "1qaz2wsx",
     database: "medical"
 });
 db.connect();
@@ -24,56 +24,62 @@ const bot = linebot({
 const linebotParser = bot.parser();
 
 app.post('/webhook', linebotParser, function (req, res) {
-    console.log('webhook in')
+    console.log('webhook in');
 });
 
-var appointment = [];
-var search = [];
+var appointment_time = [];
+var search_time = [];
+const new_user = [];
+
+
 bot.on('message', function (event) {
     var userinput = event.message.text;
-    if (event.message.text == '查詢') {
-        search.push(event.timestamp)
-        console.log(search.length)
-        console.log(event.message.text);
-        event.reply({ type: 'text', text: '請輸入身分證已進行查詢: ' })
-    }else if(userinput.match(/^[A-Z]\d{9}$/)&& search.length>=1){
-        db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
-            if (results.length == false) {
-                event.reply({ type: 'text', text: '輸入錯誤' });
-            }
-            else {
-                console.log(results[0])
-                console.log(results[0].Doctor);
-                event.reply({type:''})
-                event.reply([{type: 'text', text: '掛號資訊:'},{ type: 'text', text: '醫生:'+results[0].Doctor},{type: 'text', text: '科別:'+results[0].Subject},{type: 'text', text: '看診時間:'+moment(results[0].App_time).format('YYYY/MM/DD hh:mm')},{type:'text',text:'查詢成功'}]);
-            }
-        });
-        search=[]
-        console.log(search.length)
-    }
-    if (userinput == '掛號') {
-        appointment.push(event.timestamp)
-        console.log(appointment.length)
-        console.log(event.message.text)
-        event.reply({ type: 'text', text: '請問你要掛哪一科?請輸入牙科或胸腔科' });
+    var id_regex = /^[A-Z]\d{9}$/;
 
-    }else if(userinput=='牙科'||userinput=='胸腔科'){
-        event.reply({type:'text',text:'請輸入身分證字號、姓名、科別、預約時間以進行掛號:'})
-    }else if(userinput.match(/^[A-Z]\d{9}$/) && appointment.length>=1 ){
-        db.query("SELECT ID,Name,Subject,APP_time FROM `med_appointment_sub` WHERE `ID`= ?", [event.message.text], (error, results, fields) => {
+    if (userinput == "掛號") {
+        appointment_time.push(event.timestamp);
+        console.log(appointment_time);
+        event.reply('請問要掛哪一科？');
+    } else if (userinput == '牙科' || userinput == '胸腔科') {
+        event.reply('請輸入身分證字號以任是否有掛號紀錄:');
+        appointment_time.push(event.timestamp);
+        console.log(appointment_time.length);
+    } else if (appointment_time.length >= 2 && userinput.match(id_regex)) {
+        db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [userinput], (error, results, fields) => {
             if (results.length == true) {
                 event.reply({ type: 'text', text: '你有已掛號的紀錄，請輸入查詢以檢視相關資訊' });
+            } else {
+                event.reply({});
+                // db.query('insert into `med_appointment_sub` set ?', val, (error, results, fields) => {
+                //     console.log(results)
+                // });
             }
-            else if(results.length == false){
-                event.reply({type:'text',text:'https://forms.gle/ALzqmHWHHCPEE6K49'});
-            } 
         });
-        appointment=[]
-        console.log(appointment.length)
+        appointment_time = [];
+        console.log(appointment_time.length);
+    } else if (!userinput.match(id_regex) && new_user == null) {
+        console.log('wrong id value');
+        event.reply('wrong id value');
+    }
+
+    if (userinput == "查詢") {
+        search_time.push(event.timestamp);
+        console.log(search_time);
+        event.reply('請輸入身分證字號');
+    } else if (search_time.length >= 1 && userinput.match(id_regex)) {
+        db.query("SELECT * FROM `med_appointment_sub` WHERE `ID`= ?", [userinput], (error, results, fields) => {
+            if (results.length == false) {
+                console.log('wrong input');
+                event.reply("wrong input");
+            } else {
+                console.log([{ type: 'text', text: '查詢資訊:' }, { type: 'text', text: '醫生:' + results[0].Doctor }, { type: 'text', text: '科別:' + results[0].Subject }, { type: 'text', text: '看診時間:' + moment(results[0].App_time).format('YYYY/MM/DD') }]);
+                event.reply([{ type: 'text', text: '查詢資訊:' }, { type: 'text', text: '醫生:' + results[0].Doctor }, { type: 'text', text: '科別:' + results[0].Subject }, { type: 'text', text: '看診時間:' + moment(results[0].App_time).format('YYYY/MM/DD') }]);
+            }
+        });
+        search_time = [];
+        console.log(search_time.length);
     }
 });
-
-
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function () {
